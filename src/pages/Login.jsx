6e2458baa83287e16fe0f68aa9f0adecf4465a19
penielny <penielnyinaku@gmail.com>
2024-04-07 20:26:17 +0000

    something

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,15 +9,18 @@ import toast from "react-hot-toast";
 
 export default function Login() {
   const [details, setDetails] = useState({ email: "", password: "" });
-
+loading [loading,setLoading] = useState(false)
   const { login } = useAuth();
 
   const onSignin = () => {
+      setLoading(true)
     signInWithEmailAndPassword(firebase_auth, details.email, details.password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         toast.success(`You've successfully logged in`);
+
+        setLoading(false)
 
         login({
           token: user.uid,
@@ -29,13 +32,18 @@ export default function Login() {
             fullName: user.displayName,
             photoURL: user.photoURL,
           },
+
         });
+
+
       })
       .catch((error) => {
         const errorMessage = error.message;
         toast.error(errorMessage);
-      });
-  };
+
+        setLoading(false)
+
+      })
   return (
     <div className="flex flex-1 h-screen overflow-hidden w-screen">
       <div className="w-2/3 items-center justify-center flex flex-col space-y-5 bg-white">
@@ -105,17 +113,31 @@ export default function Login() {
 
             <div className="flex justify-between w-3/4 flex-col space-y-2 items-center mt-2">
               <button
+                disabled={loading}
                 type="submit"
-                className="bg-yellow-500 w-full p-4 rounded-md font-medium text-white text-sm"
+                className="bg-yellow-500 w-full flex items-center justify-center p-4 rounded-md font-medium text-white text-sm"
               >
-                Sign in
+              {loading ?  <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>: "Sign in"}
               </button>
-              <Link
-                to="/forgot-password"
-                className="text-sm leading-relaxed text-gray-400"
-              >
-                Forgotten Password?
-              </Link>
             </div>
           </form>
         </div>
